4f333def3ec717273f98cb04d38e06d6111a3d8a
c0untach <51246376+c0untach@users.noreply.github.com>
2024-06-09 13:49:40 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# postinstall
+# postinstall script v0.5
+
+# sudo check
+if [ "$EUID" -ne 0 ]
+  then echo "RUN AS ROOT!"
+  exit
+fi
+
+# install base packages
+xargs apt install -y < base_packages
