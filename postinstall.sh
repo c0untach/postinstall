a9961e913e1b582797dc294731be50aa5324204f
c0untach <51246376+c0untach@users.noreply.github.com>
2024-06-14 13:30:45 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -11,7 +11,9 @@ fi
 # add bookworm sources
 xargs echo -e >/etc/apt/sources.list < sources/bookworm-sources.list
 
-# add virtualbox sources
+# add virtualbox gpg and sources
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc| gpg --dearmor -o /etc/apt/trusted.gpg.d/vbox.gpg
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc| gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox.gpg
 xargs echo -e >/etc/apt/sources.list.d/virtualbox.list < sources/virtualbox-sources.list
 
 # apt update
