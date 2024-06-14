aa72cea7e4b83bb76590af2ed43501819c621bfd
c0untach <51246376+c0untach@users.noreply.github.com>
2024-06-14 13:55:43 +0300

    Update virtualbox_install.sh

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -5,7 +5,7 @@ usermod -aG vboxusers user
 newgrp vboxusers
 
 # dowenload virtualbox extension pack
-wget https://download.virtualbox.org/virtualbox/7.0.18/Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack /home/user/Downloads
+wget https://download.virtualbox.org/virtualbox/7.0.18/Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack -P /home/user/Downloads
 
 # install virtualbox extension pack
 vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack
