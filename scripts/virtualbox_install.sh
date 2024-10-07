2bd33a02b5606dbf1bb87677745feab1ca1a3f26
c0untach <51246376+c0untach@users.noreply.github.com>
2024-10-07 12:56:33 +0300

    Update virtualbox_install.sh

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -1,12 +1,6 @@
-# add gpg keys
-curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/vbox.gpg
-
-# add sources
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-
 # update and install
-apt update
-apt install virtualbox-7.0
+apt update -y 
+apt install virtualbox-7.0 -y
 
 # add user to vboxusers group
 usermod -aG vboxusers user
