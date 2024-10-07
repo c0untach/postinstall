513b1bf1f30b664679d03ece62e1969580098762
c0untach <51246376+c0untach@users.noreply.github.com>
2024-10-07 12:12:16 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -8,22 +8,18 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# add bookworm sources
+# add sources
 xargs echo -e >/etc/apt/sources.list < sources/bookworm-sources.list
 
 # add virtualbox gpg and sources
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc| gpg --dearmor -o /etc/apt/trusted.gpg.d/vbox.gpg
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc| gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox.gpg
-xargs echo -e >/etc/apt/sources.list.d/virtualbox.list < sources/virtualbox-sources.list
 
 # apt update
 apt update -y
 
 # install base packages
 xargs apt install -y < packages/base_packages
-
-# install virtualbox packages
-# xargs apt install -y < packages/virtualbox_packages
 
 # install clamav packages
 xargs apt install -y < packages/clamav_packages
