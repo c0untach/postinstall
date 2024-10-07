31ef5419751d5a4298045af9767565a26b842c60
c0untach <51246376+c0untach@users.noreply.github.com>
2024-10-07 13:17:57 +0300

    Update virtualbox_install.sh

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -8,6 +8,9 @@ usermod -aG vboxusers user
 # iniate shell for new group
 newgrp vboxusers
 
+# build kernel modules
+/sbin/vboxconfig
+
 # dowenload virtualbox extension pack
 wget https://download.virtualbox.org/virtualbox/7.0.18/Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack -P /home/user/Downloads
 
