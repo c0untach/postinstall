1735ae26452faab10cce4b0b5b6fdb431f38cfc1
c0untach <51246376+c0untach@users.noreply.github.com>
2025-11-12 09:41:34 +0200

    Update clamav_install.sh

--- scripts/clamav_install.sh
+++ scripts/clamav_install.sh
@@ -1,6 +1,9 @@
 # configure clamav
 sed -i -e "s/^NotifyClamd/#NotifyClamd/g" /etc/clamav/freshclam.conf
 
+# enable clamav-daemon
+systemctl is-enabled clamav-daemon && systemctl status clamav-daemon
+
 #stop clamav-freshclam service
 systemctl stop clamav-freshclam
 
@@ -9,3 +12,6 @@ freshclam
 
 #re-start clamav-freshclam service
 systemctl start clamav-freshclam
+
+# check status
+systemctl status clamav-freshclam
